08c2ff041100191f8bdf20d0e4501a6eb215400c
Sedri14 <sedri14@gmail.com>
2022-11-27 11:17:21 +0200

    router works with connection to js files

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import { openConnection } from "./sockets";
 import { route, routes, handleLocation } from "./router";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { sayHi } from "./about";
 //EVERYTHING THAT WE WRITE IN ANOTHER JS FILES SHOULD BE IMPORTED TO HERE!
 //ALL FUNCTIONS IN THE JS FILES SHOULD BE EXPORTED.
+
+$(() => {
+  $(window).on("load", () => {
+    sayHi();
+  });
+});
 
 $(() => {
   $(document).on("submit", () => {
